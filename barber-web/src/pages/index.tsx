1c71127e8d1cf7fa5b997ce88259d8d289296ee3
Felipe serpa <felipe.mago_skt@hotmail.com>
2025-01-14 18:45:14 -0300

    implementaçao do sistema frot end barbearia

---
+++ barber-web/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function home() {
+  return (
+    <>
+      <Head>
+        <title>BarberPro - Seu sistema completo</title>
+      </Head>
+      <div>
+        <h1>Teste projeto</h1>
+      </div>
+    </>
+  );
+}
