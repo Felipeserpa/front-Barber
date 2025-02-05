ff38844d41fa0766ad22f2316b6a6aacbd819d1a
Felipe serpa <felipe.mago_skt@hotmail.com>
2025-02-05 18:29:28 -0300

    implementaçao de cadastro na pasta register

--- barber-web/src/pages/register/index.tsx
+++ barber-web/src/pages/register/index.tsx
@@ -4,16 +4,23 @@ import logo from "../../../public/images/logo.svg";
 import Image from "next/image";
 import Link from "next/link";
 
-import { useState } from "react";
+import { useContext, useState } from "react";
+
+import { AuthContext } from "@/context/AuthContext";
 
 export default function register() {
+  const { signUp } = useContext(AuthContext);
+
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
-  function handleregister() {
-    alert("ooii");
+  async function handleregister() {
+    if (name === "" && email === "" && password === "") {
+      return;
+    }
+    await signUp({ name, email, password });
   }
 
   return (
