66b49ec711c95eeea1d12645b99b56979f94346f
Felipe serpa <felipe.mago_skt@hotmail.com>
2025-08-18 10:37:32 -0300

    implementçao de logout para barbearia

--- barber-web/src/pages/profile/index.tsx
+++ barber-web/src/pages/profile/index.tsx
@@ -1,52 +1,83 @@
+import { useContext, useState } from "react";
 import Head from "next/head";
-//import { GetServerSideProps } from "next";
-//import { getSession } from "next-auth/react";
-import { Flex, Text, Box, Heading, Input, Button } from "@chakra-ui/react";
-//sidebar
-import { Sidebar } from "@/components/sidebar";
-//import { api } from "@/services/apiClient";
+import { Flex, Text, Heading, Box, Input, Button } from "@chakra-ui/react";
+import { Sidebar } from "../../components/sidebar";
 
-export default function Profile() {
+import Link from "next/link";
+import { canSSRAuth } from "../../utils/canSSRAuth";
+import { AuthContext } from "../../context/AuthContext";
+import { setupAPIClient } from "../../services/api";
+
+interface UserProps {
+  id: string;
+  name: string;
+  email: string;
+  endereco: string | null;
+}
+
+interface ProfileProps {
+  user: UserProps;
+  premium: boolean;
+}
+
+export default function Profile({ user, premium }: ProfileProps) {
+  const { logoutUser } = useContext(AuthContext);
+
+  const [name, setName] = useState(user && user?.name);
+  const [endereco, setEndereco] = useState(
+    user?.endereco ? user?.endereco : ""
+  );
+
+  async function handleLogout() {
+    await logoutUser();
+  }
+
   return (
     <>
       <Head>
-        <title>Minha Conta - barberPRo</title>
+        <title>Minha Conta - BarberPRO</title>
       </Head>
       <Sidebar>
-        <Flex direction="column" align="center" justifyContent="flex-start">
+        <Flex
+          direction="column"
+          alignItems="flex-start"
+          justifyContent="flex-start"
+        >
           <Flex
-            w="1000%"
+            w="100%"
             direction="row"
-            align="center"
-            justifyContent="center"
+            alignItems="center"
+            justifyContent="flex-start"
           >
-            <Heading fontSize={"3xl"} mt={4} mr={4} color={"gray.700"}>
+            <Heading fontSize="3xl" mt={4} mb={4} mr={4} color="orange.900">
               Minha Conta
             </Heading>
           </Flex>
+
           <Flex
             pt={8}
-            pb={2}
-            background={"gray.100"}
-            maxW={"700px"}
+            pb={8}
+            background="barber.400"
+            maxW="700px"
             w="100%"
             direction="column"
-            alignItems={"center"}
+            alignItems="center"
             justifyContent="center"
           >
-            {" "}
-            <Flex direction={"column"} w="85%" p={4}>
+            <Flex direction="column" w="85%">
               <Text mb={2} fontSize="xl" fontWeight="bold" color="white">
-                Nome da Barbearia:
+                Nome da barbearia:
               </Text>
               <Input
                 w="100%"
                 background="gray.900"
-                placeholder="Nome da Barbearia"
-                size="md"
+                placeholder="Nome da sua barbearia"
+                size="lg"
                 type="text"
                 mb={3}
-              ></Input>
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
 
               <Text mb={2} fontSize="xl" fontWeight="bold" color="white">
                 Endereço:
@@ -54,37 +85,73 @@ export default function Profile() {
               <Input
                 w="100%"
                 background="gray.900"
-                placeholder="Endereço da Barbearia"
-                size="md"
+                placeholder="Endereço da barbearia"
+                size="lg"
                 type="text"
                 mb={3}
-              ></Input>
+                value={endereco}
+                onChange={(e) => setEndereco(e.target.value)}
+              />
+
+              <Button
+                w="100%"
+                mt={3}
+                mb={4}
+                bg="button.cta"
+                size="lg"
+                _hover={{ bg: "#ffb13e" }}
+              >
+                Salvar
+              </Button>
+
+              <Button
+                w="100%"
+                mb={6}
+                bg="transparent"
+                borderWidth={2}
+                borderColor="red.500"
+                color="red.500"
+                size="lg"
+                _hover={{ bg: "transparent" }}
+                onClick={handleLogout}
+              >
+                Sair da conta
+              </Button>
             </Flex>
-            <Button
-              w="100%"
-              mt={3}
-              mb={4}
-              bg="button.cta"
-              size="lg"
-              _hover={{ bg: "#ffb13e" }}
-            >
-              Salvar
-            </Button>
-            <Button
-              w="100%"
-              bg="transparent"
-              borderWidth={2}
-              borderColor="red.500"
-              color="red"
-              size="lg"
-              _hover={{ bg: "transparent" }}
-            >
-              {" "}
-              Sair da Conta
-            </Button>
           </Flex>
         </Flex>
       </Sidebar>
     </>
   );
 }
+
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+  try {
+    const apiClient = setupAPIClient(ctx);
+    const response = await apiClient.get("/me");
+
+    const user = {
+      id: response.data.id,
+      name: response.data.name,
+      email: response.data.email,
+      endereco: response.data?.endereco,
+    };
+
+    return {
+      props: {
+        user: user,
+        premium:
+          response.data?.subscriptions?.status === "active" ? true : false,
+      },
+    };
+  } catch (err) {
+    console.log(err);
+
+    return {
+      redirect: {
+        destination: "/dashboard",
+        permanent: false,
+      },
+    };
+  }
+});
